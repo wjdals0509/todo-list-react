292048c7e384eaf095a3638f92f10ad43d22ed1d
wjdals0509 <wjdals050932@gmail.com>
2023-02-08 13:18:05 +0900

    Create todo-list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Button } from "react-bootstrap";
+import styles from "./App.module.css";
 
 function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const [time, setTime] = useState("00:00");
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDo("");
+    setToDos((currentArray) => [...currentArray, toDo]);
+  };
+  const deleteBtn = (index) => {
+    setToDos(toDos.filter((item, todoIndex) => index !== todoIndex));
+  };
+  const currentTime = () => {
+    const date = new Date();
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    setTime(`${hours}시 ${minutes}분`);
+  };
+  const startTime = () => {
+    setInterval(currentTime, 1000);
+  };
+  startTime();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.center}>
+      <div className={styles.top}>
+        <div className={styles.h1}>📝오늘의 할일은 {toDos.length}개📝</div>
+        <div className={styles.time}>⏰ {time}</div>
+        <form onSubmit={onSubmit}>
+          <input
+            onChange={onChange}
+            value={toDo}
+            type="text"
+            placeholder="할일을 적어주세요"
+          />
+          <Button type="button" className={styles.btn} variant="light">
+            ➕
+          </Button>
+        </form>
+      </div>
+      <hr />
+      <div className={styles.scroll}>
+        <ul style={{ padding: 0 }}>
+          {toDos.map((item, index) => (
+            <li key={index}>
+              {item}
+              <button
+                className={styles.remove}
+                onClick={() => deleteBtn(index)}
+              >
+                ❎
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
